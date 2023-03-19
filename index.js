b549b9d9ccc06e4ee78cf943a00f0a763c105895
Simon Harlow <simonharlow11@gmail.com>
2023-03-18 17:27:57 -0700

    built skeleton structure of server repo, added assets and data files

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const cors = require("cors");
+const videoRoutes = require("./routes/videos")
+
+app.use(cors());
+
+app.use(express.static('public'))
+
+app.use((req, res, next) => {
+    console.log(`Request for path ${req.path} at ${new Date()}`);
+    next();
+});
+
+app.use("/video", videoRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
