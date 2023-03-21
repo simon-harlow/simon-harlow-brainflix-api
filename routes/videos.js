15b2b7c9c98f7e035d1926f5fb2b990e2c9ee3ec
Simon Harlow <simonharlow11@gmail.com>
2023-03-20 17:54:38 -0700

    added put request to update video likes counter, added middleware to update views counter when user switches between video, changed views and likes to numbers from strings in json file to accomodate changes

--- routes/videos.js
+++ routes/videos.js
@@ -5,11 +5,22 @@ const { v4: uuidv4 } = require("uuid")
 
 router.use(express.json());
 
-router.use((_req, _res, next) => {
-    console.log("Middleware from videos router");
-    next();
-});
+// Middleware to update the views count for a video
+function updateViews(req, res, next) {
+    const videoId = req.params.videoId;
+    const videos = readVideoDetails();
+    const videoIndex = videos.findIndex((video) => video.id === videoId);
 
+    if (videoIndex === -1) {
+        res.status(404).json({ message: "Video not found" });
+    } else {
+        const video = videos[videoIndex];
+        video.views += 1;
+        console.log("update views in else", video)
+        fs.writeFileSync("./data/video-details.json", JSON.stringify(videos));
+        next();
+    }
+}
 
 function readVideoDetails() {
     const videoDetailsJSON = fs.readFileSync("./data/video-details.json");
@@ -36,8 +47,8 @@ router.post("/", (req, res) => {
             // the rest are placeholder values
             channel: "Roisin O'Neill",
             image: "http://localhost:8080/images/imageUpload.jpg",
-            views: "1,234,567",
-            likes: "87,654",
+            views: 0,
+            likes: 0,
             duration: "4:01",
             video: "https://project-2-api.herokuapp.com/stream",
             comments: []
@@ -46,16 +57,30 @@ router.post("/", (req, res) => {
     const videos = readVideoDetails();
     videos.push(newVideo);
     fs.writeFileSync("./data/video-details.json", JSON.stringify(videos));
-    // Respond with the note that was created
     res.status(200).json(newVideo);
     }
 });
 
 // video route by video uuid
-router.get("/:videoId", (req, res) => {
+router.get("/:videoId", updateViews, (req, res) => {
     const videos = readVideoDetails();
     const singleVideo = videos.find((video) => video.id === req.params.videoId);
     res.json(singleVideo);
+});
+
+router.put("/:videoId/likes", (req, res) => {
+    const videoId = req.params.videoId;
+    const videos = readVideoDetails();
+    const videoIndex = videos.findIndex((video) => video.id === videoId);
+
+    if (videoIndex === -1) {
+        res.status(404).json({ message: "Video not found" });
+    } else {
+        const video = videos[videoIndex];
+        video.likes += 1;
+        fs.writeFileSync("./data/video-details.json", JSON.stringify(videos));
+        res.status(200).json(video);
+    }
 });
 
 // post comment to video by video uuid
